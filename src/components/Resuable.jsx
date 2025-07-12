c9521036f705a895566218ecd9445f8315839a52
Shrajja_Ghimire <shrajjaghimire@gmail.com>
2025-07-12 21:18:58 +0545

    ui

--- src/components/Resuable.jsx
+++ src/components/Resuable.jsx
@@ -9,12 +9,6 @@ const Resuable = (props) => {
         className={`absolute top-[110px] -left-23 bottom-0 h-[280px] ${props.ani} `}
       />
 
-      <img
-        src={props.card}
-        alt="Popup"
-        className="absolute top-[110px] left-[30px] h-[280px] opacity-0 transition-all duration-500 peer-checked:opacity-100"
-      />
-
       <div className="ml-[180px] mt-7">
         <p className="text-right w-[345px] font-bold text-2xl">{props.title}</p>
         <p className=" text-right w-[345px] leading-1 mt-7">{props.name}</p>
